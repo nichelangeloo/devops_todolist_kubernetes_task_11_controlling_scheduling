659da7cf771fe83d829e46d6f96a51b54d15d090
Nikita Ivanichenko <importantthings1482@gmail.com>
2026-06-08 19:45:37 +0300

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+kind create cluster --config cluster.yml
+kubectl taint nodes kind-worker kind-worker2 app=mysql:NoSchedule
 kubectl apply -f .infrastructure/mysql/ns.yml
 kubectl apply -f .infrastructure/mysql/configMap.yml
 kubectl apply -f .infrastructure/mysql/secret.yml
